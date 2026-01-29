7d1ec10d0d4038fec5020212c91336b4a3879ed6
semagllclkk <segece3308@gmail.com>
2026-01-29 14:31:00 +0300

    fix: declare process global directly in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
-/// <reference types="node" />
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+declare const process: { env: { PORT?: string } };
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
